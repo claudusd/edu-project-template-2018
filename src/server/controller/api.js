be473b8fe76c8c11a32aab4fedba171de7196ed8
LOURADOUR Emeric <emlouradou@dpt_info.iut.local>
2018-01-30 17:10:58 +0100

    dal en cours

--- src/server/controller/api.js
+++ src/server/controller/api.js
@@ -25,22 +25,51 @@ router.post('/', function (request, response) {
  Url : /api/episodes
  Description : Get List of all Episodes
  */
-router.get('/api/episodes', function (request, response) {
+router.get('/', function (request, response) {
     var episodes = repository.findAll();
     response.status(200);
     response.send(episodes);
 });
 
 /*
-Method : GET
-Url : /api/episodes/id
-Description : Get Episode
+ Method : GET
+ Url : /api/episodes/id
+ Description : Get Episode
  */
 
-router.get('/', function (request, response) {
+router.get('/:idEp', function (request, response) {
     var epi = repository.findBy(request.params.id);
     response.status(200);
     response.send(epi);
+});
+
+
+router.delete('/:idEp', function(req,res){
+    var idEp = req.params.idEp;
+    var path = config.data + "/" + idEp + ".json";
+    if(fs.existsSync(path)){
+        fs.unlinkSync(path);
+        return res.sendStatus(204);
+    }else {
+        return res.sendStatus(404);
+    }
+});
+
+router.put('/:idEp', function(req,res){
+    var idEp = req.params.idEp;
+    var path = config.data + "/" + idEp + ".json";
+    var episode = req.body;
+    episode.name = req.body.name;
+    episode.code = req.body.code;
+    episode.note = req.body.note;
+    episode.id = idEp;
+    if(fs.existsSync(path)){
+        fs.writeFile(path, JSON.stringify(episode), function() {
+            res.sendStatus(200);
+        });
+    }else {
+        return res.sendStatus(404);
+    }
 });
 
 module.exports = router;
--- src/server/repository/Repository.js
+++ src/server/repository/Repository.js
@@ -9,6 +9,7 @@ class Repository {
 
     }
 
+    /*//GET all episode action
     findAll() {
         var episodes = [];
         var listFiles = fs.readdirSync(config.data)
@@ -17,8 +18,27 @@ class Repository {
             episodes.push(epi);
         });
         return episodes;
+    }*/
+    findAll(){
+        return new Promise((resolve,reject)=> {
+            const path = config.data
+            fs.readdir(path, function (err, datas)=> {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                var episodes = []
+                for(var i=0;i<datas.length(); i++){
+                    episodes.push(JSON.parse(data[i]));
+                }
+                resolve(episodes)
+            });
+        });
     }
 
+
+
+    /*//GET episode action
     findBy(id) {
         var epi = -1;
         var listFiles = fs.readdirSync(config.data)
@@ -32,17 +52,57 @@ class Repository {
             response.status(404);
         }
     }
+    */
+    findBy(id){
+        return new Promise((resolve,reject)=>{
+            const path= config.data+"/"+id+".json";
+           fs.readFile(path,"utf-8",function (err,data){
+               if(err){
+                   reject(err);
+                   return;
+               }
+               resolve(JSON.parse(data));
+           });
 
+        });
+    }
+
+    /*//POST episode action
     add(object) {
         var episode = object;
         episode.id = episode.name.replace(/ /g, '') + episode.code;
         fs.writeFileSync(config.data + '/' + episode.id + '.json', JSON.stringify(episode));
         return episode;
+    }*/
+    add(episode){
+        return new Promise((resolve,reject)=>{
+           const path=config.data+"/"+episode.id+".json"
+            fs.writeFile(path,Json.stringify(episode),function(err,data){
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve(episode);
+            })
+        });
     }
 
+    //PUT action
     edit(id, newValues) {
 
     }
+
+    //DELETE episode action
+    delete(object){
+        var path = config.data + "/" + object.idEp + ".json";
+        if(fs.existsSync(path)){
+            fs.unlinkSync(path);
+            return res.sendStatus(204);
+        }else {
+            return res.sendStatus(404);
+        }
+    }
+
 }
 
 var repository = new Repository();
